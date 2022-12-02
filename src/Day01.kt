f9eee0d1aba471f00e58d2f4a08067b4f879f6ff
sylvain <8940871+punx120@users.noreply.github.com>
2022-12-02 07:42:24 -0500

    Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
-    }
+        var max = 0
+        var current = 0
 
-    fun part2(input: List<String>): Int {
-        return input.size
+        for (row: String in input) {
+            if (row.isEmpty()) {
+                if (current > max) {
+                    max = current
+                }
+                current = 0
+            } else {
+                current += row.toInt()
+            }
+        }
+        return max
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
 }
