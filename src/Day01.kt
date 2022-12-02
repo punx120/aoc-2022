332cc1286bbdca0bf4f00440f63a33518d5296f0
sylvain <8940871+punx120@users.noreply.github.com>
2022-12-02 07:48:57 -0500

    Day01.kt - Add part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -16,10 +16,30 @@ fun main() {
         return max
     }
 
+    fun part2(input: List<String>): Int {
+        var current = 0
+        val top = mutableListOf<Int>()
+
+        for (row: String in input) {
+            if (row.isEmpty()) {
+                top.add(current)
+                if (top.size > 3) {
+                    top.sort()
+                    top.removeAt(0)
+                }
+                current = 0
+            } else {
+                current += row.toInt()
+            }
+        }
+        return top.sum()
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     println(part1(testInput))
 
     val input = readInput("Day01")
     println(part1(input))
+    println(part2(input))
 }
